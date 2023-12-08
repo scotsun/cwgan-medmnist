f71e0559bec06d62f444d04037bf93f7693c8fbc
ireneisme <75598096+ireneisme@users.noreply.github.com>
2023-12-08 14:39:05 -0500

    Update experiment_utils.py
    Add compare_all _result function which is used for drawing the results on all data sets

--- experiment_utils.py
+++ experiment_utils.py
@@ -80,3 +80,55 @@ def compare_real_fake_by_class(
         plt.axis("off")
         plt.show()
     return
+
+def overall_comparison_by_class(
+    data_flag, 
+    real_dataloader, 
+    label_dict, 
+    n_channels, 
+    n_samples=4, 
+    grid_nrow=16
+):
+    img_batch, label_batch = next(iter(real_dataloader))
+    img_batch = img_batch * 0.5 + 0.5
+    n_classes = len(label_dict)
+    root_path = '/content/model/'
+    result_path = '/content/results/'
+    plt.figure()
+    plt.tight_layout(pad=1.08, h_pad=None, w_pad=None, rect=None)
+    f, axarr = plt.subplots((n_classes+1)//2, 2, figsize=(16, 15)) 
+
+    for i in range(n_classes):
+        label = label_dict[i]
+        idx = torch.where(label_batch == i, 1, 0).reshape(-1).nonzero().reshape(-1)
+        real = img_batch[idx][:grid_nrow]
+        all_image_in_one_class = torch.zeros(grid_nrow,real.shape[1], real.shape[2], real.shape[3])
+        all_image_in_one_class[:real.shape[0],:,:,:] = real
+
+        for model in range(2):
+            if model == 0:
+                gan = model_vanila
+                model_type = 'vanilla'
+            else:
+                gan = model_wgan
+                model_type = 'wgan'
+            for epoch in [10, 100]:
+                for embedding in [0, 4, 8, 32]:
+                    if embedding == 0:
+                        model_path = root_path + f'{data_flag}_OneHot_epo{epoch}_{model_type}.pt'
+                    else:
+                        model_path = root_path + f'{data_flag}_emb{embedding}_epo{epoch}_{model_type}.pt'
+                    # print(model_path)
+                    gan.G = torch.load(model_path, map_location=device)
+                    samples = gan.generate_img(n_samples, i, n_channels)
+                    all_image_in_one_class = torch.cat((all_image_in_one_class, samples), 0)
+        axarr[i//2, i%2].imshow(
+            torch.permute(make_grid(all_image_in_one_class, nrow=grid_nrow, padding=1), (1,2,0)),
+        )
+        axarr[i//2, i%2].axis("off")
+        axarr[i//2, i%2].title.set_text(f'{i}: {label_dict[i]}')
+#         axarr[i//2, i%2].savefig(result_path + f'{data_flag}_class{i}.png')
+    plt.axis("off")
+    plt.savefig(result_path + f'{data_flag}_combine.png')
+    plt.show()
+    return
